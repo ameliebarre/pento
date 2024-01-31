74bc5855483a50fcc45eaa89f9b155dfc8856097
Amelie <amelie.barre.co@gmail.com>
2024-01-31 22:29:40 +0100

    create category form to create categories

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import LoginForm from "@/components/LoginForm";
+import LoginForm from "@/components/forms/LoginForm";
 
 export default function Login() {
   return (
