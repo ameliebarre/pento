3406103ab4b86dfb1bea7b80d678acbec2c50025
Amelie Barre <amliebarre@gmail.com>
2025-02-03 22:49:14 +0100

    create and display header and footer sections

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,39 @@
+import { ShoppingCart, UserIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
+
+const Header = () => {
+  return (
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between'>
+        <div className='flex-start'>
+          <Link href='/' className='flex-start'>
+            <Image
+              src='/images/logo.svg'
+              alt={`${APP_NAME} logo`}
+              width={180}
+              height={51}
+              priority={true}
+            />
+          </Link>
+        </div>
+        <div className='space-x-2'>
+          <Button asChild variant='ghost'>
+            <Link href='/cart'>
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant='ghost'>
+            <Link href='/sign-in'>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
