364a60fe076bd5c8ce806b004591e8e4dcd95c9a
Amelie Barre <amliebarre@gmail.com>
2025-02-05 12:29:53 +0100

    display responsive sheet menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,14 +1,19 @@
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
+import { ShoppingCart } from 'lucide-react';
+import { UserRoundIcon } from 'lucide-react';
+import Menu from './menu';
+import ButtonLink from '../buttonLink';
 
 const Header = () => {
   return (
     <header className='h-18 border-b flex items-center justify-center'>
       <div className='wrapper flex-between'>
-        <div className='flex-start'>
+        <div className='flex-start gap-3 md:gap-8'>
+          <div className='md:hidden'>
+            <Menu />
+          </div>
           <Link href='/' className='flex-start'>
             <Image
               src='/images/logo.svg'
@@ -18,19 +23,29 @@ const Header = () => {
               priority={true}
             />
           </Link>
+          <nav className='hidden md:flex'>
+            <ul className='list-none flex flex-row gap-6'>
+              <li className='hover:underline underline-offset-8'>
+                <Link href='/products'>Products</Link>
+              </li>
+              <li>
+                <Link href='/showcase'>Showcase</Link>
+              </li>
+              <li>
+                <Link href='/contact'>Contact</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className='space-x-2'>
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild variant='ghost'>
-            <Link href='/sign-in'>
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <nav className='flex-end flex gap-6'>
+          <ButtonLink href='/cart'>
+            <ShoppingCart className='!w-6 !h-6 md:!w-4 md:!h-4' />
+            <span className='hidden md:inline'>Cart</span>
+          </ButtonLink>
+          <ButtonLink href='/sign-in' className='hidden md:flex'>
+            <UserRoundIcon /> Sign In
+          </ButtonLink>
+        </nav>
       </div>
     </header>
   );
